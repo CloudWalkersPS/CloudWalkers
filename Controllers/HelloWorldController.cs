1307409a4d09b258163442360199190a51c8f821
Aditya Mishra <84185824+08-Aditya-Mishra@users.noreply.github.com>
2023-02-25 19:07:53 +0530

    Initial Commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello", "World!" };
+        }
+    }
+}
